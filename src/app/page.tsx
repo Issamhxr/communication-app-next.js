50fb272fba47dd75b25cb47548c9f53f2d55d44a
issam <merighedissameddin@gmail.com>
2025-02-01 23:35:34 +0100

    sso sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import { Button } from "@/components/ui/button";
+import SwitchMode from "@/components/ui/SwitchMode";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      yooo
+    <div className="m-5">
+        <SignedOut>
+            <SignInButton  mode="modal"> 
+            <Button> Sign in </Button>
+             </SignInButton>
+         
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          
+          <SwitchMode/>
     </div>
   );
 }
